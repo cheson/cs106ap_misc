0bb6a6cde2f24b5bf2cd4991e3b0203716306ffa
Jason Chen <cheson@stanford.edu>
2018-01-25 14:57:14 -0800

    First commit, added python -> html pyg script

---
+++ pyg.py
@@ -0,0 +1,31 @@
+# pip install pygments
+# python pyg.py code_file
+
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+
+import sys
+
+code_file = sys.argv[1]
+output_name = code_file.split(".")[0]
+output_html = output_name + ".html"
+output_css = output_name + ".css"
+
+f_in = open(sys.argv[1], "r")
+
+code = f_in.read()
+
+f_out_html = open(output_html, "w")
+f_out_css = open(output_css, "w")
+
+head = '''
+<head>
+  <link rel="stylesheet" href="''' + output_css + '''">
+</head>
+'''
+
+f_out_html.write(head)
+f_out_html.write(highlight(code, PythonLexer(), HtmlFormatter()))
+
+f_out_css.write(HtmlFormatter().get_style_defs('.highlight'))
